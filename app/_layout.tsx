f3b4d0ab66df1204302bd98ebd33ae0cb92875b6
cBasique <maelroulette12@gmail.com>
2025-07-29 10:14:31 +0200

    Nativewind setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,42 @@
-import { Stack } from "expo-router";
-import * as Sentry from '@sentry/react-native';
+import { SplashScreen, Stack } from "expo-router";
+import * as Sentry from "@sentry/react-native";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import "./globals.css";
 
 Sentry.init({
-  dsn: 'https://659bf5ca13cce02d2e0a6e42d57d1f9d@o4509674390683648.ingest.de.sentry.io/4509750754934864',
+	dsn: "https://659bf5ca13cce02d2e0a6e42d57d1f9d@o4509674390683648.ingest.de.sentry.io/4509750754934864",
 
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
+	// Adds more context data to events (IP address, cookies, user, etc.)
+	// For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+	sendDefaultPii: true,
 
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+	// Configure Session Replay
+	replaysSessionSampleRate: 1,
+	replaysOnErrorSampleRate: 1,
+	integrations: [
+		Sentry.mobileReplayIntegration(),
+		Sentry.feedbackIntegration(),
+	],
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
-  return <Stack />;
+	const [fontsLoaded, error] = useFonts({
+		"CalSans-Regular": require("../assets/fonts/CalSans-Regular.ttf"),
+		"Sora-Regular": require("../assets/fonts/Sora-Regular.ttf"),
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+		if (fontsLoaded) SplashScreen.hideAsync();
+	}, [fontsLoaded, error]);
+
+	if (!fontsLoaded) return null;
+
+	return <Stack screenOptions={{ headerShown: false }} />;
 });
+
+Sentry.showFeedbackWidget();
