d296a2072e6df35e911c28b22d775eea182bc419
cBasique <maelroulette12@gmail.com>
2025-07-30 15:32:23 +0200

    Progression stats goals

--- components/ProgressOverview.tsx
+++ components/ProgressOverview.tsx
@@ -1,8 +1,15 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
 import PrimaryGradient from "./PrimaryGradient";
+import { useGoalsStore } from "@/store";
 
 const ProgressOverview = () => {
+	const { goals } = useGoalsStore();
+	const finishGoalsNumber = useMemo(
+		() => goals.filter((goal) => goal.state === "finish").length,
+		[goals]
+	);
+
 	return (
 		<PrimaryGradient>
 			<View className='w-full p-5 flex-col gap-5'>
@@ -11,14 +18,16 @@ const ProgressOverview = () => {
 				</Text>
 				<View className='w-full flex-row gap-3'>
 					<View className='flex-1 h-full bg-background/20 rounded-lg p-4 flex-col gap-1'>
-						<Text className='text-3xl text-background font-calsans'>12</Text>
+						<Text className='text-3xl text-background font-calsans'>0</Text>
 						<Text className='text-base text-background font-sregular'>
 							Séances complétées
 						</Text>
 					</View>
 
 					<View className='flex-1 h-full bg-background/20 rounded-lg p-4 flex-col gap-1'>
-						<Text className='text-3xl text-background font-calsans'>3</Text>
+						<Text className='text-3xl text-background font-calsans'>
+							{finishGoalsNumber}
+						</Text>
 						<Text className='text-base text-background font-sregular'>
 							Objectifs atteints
 						</Text>
