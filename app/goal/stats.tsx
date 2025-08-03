0516e97644e31dc4d8eb45f97dcb77102a096621
Mael <maelroulette12@gmail.com>
2025-08-03 15:45:43 +0200

    fix header stats

--- app/goal/stats.tsx
+++ app/goal/stats.tsx
@@ -1,22 +1,14 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
 import { useGoalsStore } from "@/store";
+import React from "react";
+import { ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import GoalStats from "./components/GoalStats";
-import { Link } from "expo-router";
-import AntDesign from "@expo/vector-icons/AntDesign";
 
 const Stats = () => {
 	const { goals } = useGoalsStore();
 
 	return (
-		<SafeAreaView className='pt-8 bg-background min-h-full flex-1'>
-			<View className='mb-8 px-5 flex-row items-center justify-start'>
-				<Link href={"/profile"} className='mr-4'>
-					<AntDesign name='caretleft' size={24} color='#132541' />
-				</Link>
-				<Text className='title'>Statistiques</Text>
-			</View>
+		<SafeAreaView className='bg-background min-h-full flex-1'>
 			<ScrollView className='px-5'>
 				<View className='flex-col gap-5'>
 					{goals.map((goal) => (
