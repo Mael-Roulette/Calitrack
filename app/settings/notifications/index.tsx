79e3ce9091ef733c8e767a46cd9a6daa0f900b0d
cBasique <maelroulette12@gmail.com>
2025-09-04 15:56:45 +0200

    remove unused Switch component and clean up notifications settings layout

--- app/settings/notifications/index.tsx
+++ app/settings/notifications/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Switch } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const Index = () => {
@@ -6,15 +6,7 @@ const Index = () => {
 		<SafeAreaView className='flex-1 px-5 bg-background'>
 			<View className='mb-8'>
 				<Text className='text-primary font-calsans text-xl'>Entrainements</Text>
-				<View className='flex-row items-center justify-between'>
-					<Text className='text-lg'>
-						Authentification à deux facteurs (A2F)
-					</Text>
-					<Switch />
-				</View>
-			</View>
-			<View>
-				<Text className='text-primary font-calsans text-xl'>Objectfis</Text>
+				{/* TODO: Ajouter les notifications */}
 			</View>
 		</SafeAreaView>
 	);
